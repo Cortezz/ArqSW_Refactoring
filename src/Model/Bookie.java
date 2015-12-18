c807162934715be2e936f8f277e4ee99401e3e59
Cortez <Cortez@Cortez-pc>
2015-12-18 23:18:12 +0000

    Bookies don't have subscribed events now.

--- src/Model/Bookie.java
+++ src/Model/Bookie.java
@@ -9,44 +9,24 @@ import java.util.HashMap;
 public class Bookie {
     
     private String nome;
-    private ListaEventos eventosSubscritos;
     
     /**
      * Empty constructor.
      */
     public Bookie (){
         nome = "";
-        eventosSubscritos = new ListaEventos();
     }
     
     public Bookie (String nome){
         this.nome =nome;
-        eventosSubscritos = new ListaEventos();
     }
     
     
     
     /*Getters*/
-    public HashMap<Integer,Evento> getEventosSubscritos(){ return eventosSubscritos.getListaEventos();}
     public String getNome () {return this.nome;}
     /*Setters*/
     public void setNome (String nome) {this.nome = nome;}
-    
-    /***
-     * The bookie subscribes to an event.
-     * @param e 
-     */
-    public void subscreveEvento (Evento e){
-        eventosSubscritos.addEvento(e.getID(), e);
-    }
-    
-    /**
-     * Removes the subscription from a certain event, given its ID.
-     * @param evID ID of the event.
-     */
-    public void removeSubscricao (int evID){
-        eventosSubscritos.removeEvento(evID);
-    }
     
     
     @Override
@@ -54,7 +34,7 @@ public class Bookie {
         if (this==o) return true;
         if (o==null || o.getClass()!=this.getClass()) return false;
         Bookie b = (Bookie)o;
-        return (this.nome.equals(b.getNome()) && eventosSubscritos.listaEventosEquals(b.getEventosSubscritos()));
+        return (this.nome.equals(b.getNome()));
     }
     
 }
--- test/Model/BookieTest.java
+++ test/Model/BookieTest.java
@@ -41,21 +41,6 @@ public class BookieTest {
     public void tearDown() {
     }
 
-    /**
-     * Test of getEventosSubscritos method, of class Bookie.
-     */
-    @Test
-    public void testGetEventosSubscritos() {
-        System.out.println("getEventosSubscritos");
-        Evento e1 = new Evento("FC Porto","SC Braga", new Date(1241));
-        //New HashMap with an event in it
-        ListaEventos eventos1 = new ListaEventos();
-        eventos1.addEvento(e1.getID(), e1);
-        //Create b and subscrite to an event.
-        Bookie b = new Bookie("Rodrigo");
-        b.subscreveEvento(e1);
-        assertEquals(eventos1.getListaEventos(), b.getEventosSubscritos());
-    }
 
    
 
@@ -70,77 +55,10 @@ public class BookieTest {
         assertEquals(instance.getNome(),"Filipe");
     }
 
-    /**
-     * Test of subscreveEvento method, of class Bookie.
-     */
-    @Test
-    public void testSubscreveEvento() {
-        System.out.println("subscreveEvento");
-        Bookie instance = new Bookie("Leão");
-        
-        Evento e1 = new Evento("FC Porto","SC Braga", new Date(1241));
-        e1.setBookieCriouEvento(instance);
-        instance.subscreveEvento(e1);
-        Evento e2 = new Evento("FC Porto","SC Braga", new Date(1241));
-        e2.setBookieCriouEvento(instance);
-        Evento e3 = instance.getEventosSubscritos().get(e1.getID());
-        e3.setID(e2.getID());
-        //Minor details
-        e2.setResultadoFinal(Evento.Resultado.VITORIA);
-        e3.setResultadoFinal(Evento.Resultado.VITORIA);
-        e2.setOdds(1, 2, 3);
-        e3.setOdds(1, 2, 3);
-        
-        
-        
-        assertEquals(e2,e3);
-    }
     
-    /**
-     * 2nd Test of subscreveEvento method, of class Bookie.
-     */
-    @Test
-    public void testSuscreveEvento2(){
-        System.out.println("subscreveEvento");
-        Evento e1 = new Evento("FC Porto","SC Braga", new Date(1241));
-        Bookie instance = new Bookie("Leão");
-        instance.subscreveEvento(e1);
-        assertEquals(instance.getEventosSubscritos().size(),1);
-        
-    }
-
-    /**
-     * Test of removeSubscricao method, of class Bookie.
-     */
-    @Test
-    public void testRemoveSubscricao() {
-        System.out.println("removeSubscricao");
-        Evento e1 = new Evento("FC Porto","SC Braga", new Date(1241));
-        Evento e2 = new Evento("FC Porto","SC Braga", new Date(1241));
-        Bookie instance = new Bookie("Leão");
-        instance.subscreveEvento(e2);
-        instance.subscreveEvento(e1);
-        instance.removeSubscricao(e1.getID());
-        Evento e3 = instance.getEventosSubscritos().get(e2.getID());
-        assertEquals(e3,e2);
-    }
     
-    /**
-     * Test of removeSubscricao method, of class Bookie.
-     */
-    @Test
-    public void testRemoveSubscricao2 (){
-        System.out.println("removeSubscricao");
-        Evento e1 = new Evento("FC Porto","SC Braga", new Date(1241));
-        Evento e2 = new Evento("FC Porto","SC Braga", new Date(1241));
-        Bookie instance = new Bookie("Leão");
-        instance.subscreveEvento(e2);
-        instance.subscreveEvento(e1);
-        instance.removeSubscricao(e1.getID());
-        assertEquals(instance.getEventosSubscritos().size(),1);
-        
-    }
 
+  
     /**
      * Test of equals method, of class Bookie.
      */
@@ -151,60 +69,7 @@ public class BookieTest {
         Bookie b1 = new Bookie("João");
         Bookie b2 = new Bookie();
         b2.setNome("João");
-        b1.subscreveEvento(e1);
-        b2.subscreveEvento(e1);
         assertEquals(b1,b2);
-    }
-    
-    /**
-     * 2nd Test of equals method, of class Bookie.
-     */
-    @Test
-    public void testEquals2() {
-        System.out.println("equals");
-        Evento e1 = new Evento("FC Porto","SC Braga", new Date(1241));
-        Bookie b1 = new Bookie("João");
-        Bookie b2 = new Bookie();
-        b2.setNome("João");
-        b1.subscreveEvento(e1);
-        b2.subscreveEvento(e1);
-        b2.removeSubscricao(e1.getID());
-        b2.subscreveEvento(e1);
-        assertEquals(b1,b2);
-    }
-    
-    /**
-     * 3rd Test of equals method, of class Bookie.
-     */
-    @Test
-    public void testEquals3() {
-        System.out.println("equals");
-        Evento e1 = new Evento("FC Porto","SC Braga", new Date(1241));
-        Bookie b1 = new Bookie("João");
-        Bookie b2 = new Bookie();
-        b2.setNome("João");
-        b1.subscreveEvento(e1);
-        b2.subscreveEvento(e1);
-        b1.removeSubscricao(e1.getID());
-        assertFalse("This should be false.", b1.equals(b2));
-    }
-    
-    /**
-     * 4th Test of equals method, of class Bookie.
-     */
-    @Test
-    public void testEquals4() {
-        System.out.println("equals");
-        Evento e1 = new Evento("FC Porto","SC Braga", new Date(1241));
-        Bookie b1 = new Bookie("João");
-        Bookie b2 = new Bookie();
-        b2.setNome("João");
-        b1.subscreveEvento(e1);
-        b2.subscreveEvento(e1);
-        b1.removeSubscricao(e1.getID());
-        Evento e3 = new Evento("FC Porto","SC Braga", new Date());
-        b1.subscreveEvento(e3);
-        assertFalse("This should be false.", b1.equals(b2));
     }
     
 }
